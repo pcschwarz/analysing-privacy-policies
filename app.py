6002190716d44d23bdff57ee84495cde3d08e81d
unknown <pasch@mail.de>
2022-01-05 17:50:36 +0100

    removed unnecessary requirements

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import sys
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 import dash_table
 import flask
 import plotly.express as px
